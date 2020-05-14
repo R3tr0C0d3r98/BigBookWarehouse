757ee33297f756a6fd37790837e1cebd57cc9a4b
AJ <AJ@THE-ANIMUS>
2020-05-14 14:00:06 +0100

    Working code with Partial Classes

---
+++ Pages/BookPage.razor.cs
@@ -0,0 +1,33 @@
+﻿using BigBookWarehouse.Core.Services.Books;
+using BigBookWarehouse.DAL.Models.Books;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BigBookWarehouse.Web.Pages
+{
+    public partial class BookPage : ComponentBase
+    {
+        [Inject]
+        public IBookService BookService { get; set; }
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Parameter]
+        public int Id { get; set; }
+
+        private Book book;
+
+        protected override void OnInitialized()
+        {
+            book = BookService.GetBook().FirstOrDefault(x => x.Id == Id);
+            if (book == null)
+            {
+                NavigationManager.NavigateTo("/");
+                return;
+            }
+        }
+    }
+}
---
+++ Pages/RoutingPage.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BigBookWarehouse.Web.Pages
+{
+    public partial class RoutingPage : ComponentBase
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Parameter]
+        public int Id { get; set; }
+        private void GoToNextIndex()
+        {
+            NavigationManager.NavigateTo($"/routing/{Id + 1}");
+        }
+    }
+}
