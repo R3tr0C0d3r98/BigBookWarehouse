3b8f70dcc73ba56e72ca945e05adf97ff4dea220
AJ <AJ@THE-ANIMUS>
2020-05-14 16:43:03 +0100

    Tables added with a linked page

---
+++ BigBookWarehouse.Core.Web/Books/BookTableComponent.razor.cs
@@ -0,0 +1,14 @@
+﻿using BigBookWarehouse.DAL.Models.Books;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigBookWarehouse.Core.Web.Books
+{
+    public partial class BookTableComponent : ComponentBase
+    {
+        [Parameter]
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
---
+++ BigBookWarehouse/Pages/BooksPage.razor.cs
@@ -0,0 +1,23 @@
+﻿using BigBookWarehouse.Core.Services.Books;
+using BigBookWarehouse.DAL.Models.Books;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BigBookWarehouse.Web.Pages
+{
+    public partial class BooksPage : ComponentBase
+    {
+        [Inject]
+        public IBookService BookService { get; set; }
+
+        private Book[] books;
+
+        protected override void OnInitialized()
+        {
+            books = BookService.GetBook();
+        }
+    }
+}
